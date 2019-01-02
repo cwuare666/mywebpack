ad8b46941a0e23a5dd241f4c577bda5dc2f902d7
chenwei <@chenwei>
2019-01-02 14:47:09 +0800

    添加了test01借鉴demo

---
+++ test01/src/index.tsx
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import { ConnectedRouter } from 'connected-react-router';
+import { Route, Switch, Redirect } from 'react-router-dom';
+import { LocaleProvider } from 'antd';
+import zh_CN from 'antd/lib/locale-provider/zh_CN';
+import Routes from '@router/router';
+import store, { history } from '@store/store';
+
+import styles from './app.less';
+import "normalize.css";
+
+const App = {
+	run: function (Component) {
+		render(
+			<Provider store={store}>
+				<ConnectedRouter history={history}>
+					<LocaleProvider locale={zh_CN}>
+						<Component />
+					</LocaleProvider>
+				</ConnectedRouter>
+			</Provider>,
+			document.getElementById('root') as HTMLElement
+		)
+	}
+}
+
+App.run(Routes);
